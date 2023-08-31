94bcbe47e180df0cc3cf67dc39e7508d7b86c56e
Felex Onyango <felex.onyango@sibasi.ltd>
2023-08-31 15:27:47 +0300

    updated

---
+++ web/dist/coreui-free-angular-admin-template/common.8f8d8238249f3a4e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcoreui_free_angular_admin_template=self.webpackChunkcoreui_free_angular_admin_template||[]).push([[592],{8779:(v,s,e)=>{e.d(s,{AK:()=>u,AL:()=>o});var t=e(9653),l=e(1234),n=e(9119);const _=(0,t.ZF)("leave"),a=l.VL.getSelectors(),c=(0,t.P1)(_,a.selectEntities),u=(0,t.P1)(_,a.selectAll),o=(0,t.P1)(c,n.rk,(r,E)=>r?r[E?.params.id]:null)}}]);
